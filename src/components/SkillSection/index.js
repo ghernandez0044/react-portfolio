f015263cafc17014f81d881b650114c4b9a81b6e
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-10 03:12:04 -0700

    progress check, still working on skill section component

--- src/components/SkillSection/index.js
+++ src/components/SkillSection/index.js
@@ -1,8 +1,14 @@
-function SkillSection(){
+import LineGradient from "../LineGradient"
+import useMediaQuery from "../../hooks/useMediaQuery"
+import { motion } from "framer-motion"
 
+function SkillSection(){
+    const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)')
 
     return (
-        null
+        <section id='skills' className="pt-1p pb-24">
+            {/* HEADER AND IMAGE SECTION */}
+        </section>
     )
 }
 
