84c39e98fddaf7bf5f53cdcf9e11836a04f59777
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-09 20:26:48 -0700

    progress check, still working on navigation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import useMediaQuery from './hooks/useMediaQuery'
+import Navigation from "./components/Navigation";
+
 function App() {
+  // Create state variables
+  const [ selectedPage, setSelectedPage ] = useState('home')
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+
   return (
-    <div className="App">
-     
+    <div className="app bg-space-blue">
+      <Navigation selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,37 @@
+import { useState } from "react"
+import AnchorLink from "react-anchor-link-smooth-scroll"
+import useMediaQuery from "../../hooks/useMediaQuery"
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+    const lowerCasePage = page.toLowerCase()
+    return (
+        <AnchorLink className={`${selectedPage === lowerCasePage ? 'text-tiffany-blue' : ''} hover:text-tiffany-blue transition duration-500`} href={`#${lowerCasePage}`} onClick={() => setSelectedPage(lowerCasePage)}>{page}</AnchorLink>
+    )
+}
+
+function Navigation({ selectedPage, setSelectedPage }){
+    // Create state variables
+    const [ isMenuToggled, setIsMenuToggled ] = useState(false)
+    const isAboveSmallScreens = useMediaQuery("(min-width: 768px)")
+
+    return (
+        <nav className={`border-2 border-lime-600 z-40 w-full fixed top-0 py-6`}>
+            <div className="flex items-center justify-between mx-auto w-5/6">
+                <h4 className="font-subheading text-3xl font-bold">LOGO</h4>
+
+            {/* DESKTOP NAVIGATION */}
+            {isAboveSmallScreens ? (
+                <div className="flex justify-between gap-16 font-subheading text-sm font-semibold">
+                    <Link page='Home' selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+                </div>
+            ) : (
+                <div>
+
+                </div>
+            )}
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
