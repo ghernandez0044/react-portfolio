98fe23aefd9aa715f9b9b71c7512c5f7298c4823
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-16 01:03:05 -0700

    fixed dot group component

--- src/components/DotGroup/index.js
+++ src/components/DotGroup/index.js
@@ -7,11 +7,11 @@ function DotGroup({ selectedPage, setSelectedPage }){
 
     return (
         <div className="flex flex-col gap-6 fixed top-[60%] right-7">
-            <AnchorLink className={`${selectedPage === 'home' ? `${selectedStyle}` : 'bg-pale-purple'} hover:text-tiffany-blue transition duration-500 w-3 h-3 rounded-full`} href='#home' onClick={() => setSelectedPage('home')} />
-            <AnchorLink className={`${selectedPage === 'skills' ? `${selectedStyle}` : 'bg-pale-purple'} hover:text-tiffany-blue transition duration-500 w-3 h-3 rounded-full`} href='#skills' onClick={() => setSelectedPage('skills')} />
-            <AnchorLink className={`${selectedPage === 'projects' ? `${selectedStyle}` : 'bg-pale-purple'} hover:text-tiffany-blue transition duration-500 w-3 h-3 rounded-full`} href='#projects' onClick={() => setSelectedPage('projects')} />
-            <AnchorLink className={`${selectedPage === 'resume' ? `${selectedStyle}` : 'bg-pale-purple'} hover:text-tiffany-blue transition duration-500 w-3 h-3 rounded-full`} href='#resume' onClick={() => setSelectedPage('resume')} />
-            <AnchorLink className={`${selectedPage === 'contact' ? `${selectedStyle}` : 'bg-pale-purple'} hover:text-tiffany-blue transition duration-500 w-3 h-3 rounded-full`} href='#contact' onClick={() => setSelectedPage('contact')} />
+            <AnchorLink className={`${selectedPage === 'home' ? selectedStyle : 'bg-pale-purple'} w-3 h-3 rounded-full`} href='#home' onClick={() => setSelectedPage('home')} />
+            <AnchorLink className={`${selectedPage === 'skills' ? selectedStyle : 'bg-pale-purple'} w-3 h-3 rounded-full`} href='#skills' onClick={() => setSelectedPage('skills')} />
+            <AnchorLink className={`${selectedPage === 'projects' ? selectedStyle : 'bg-pale-purple'} w-3 h-3 rounded-full`} href='#projects' onClick={() => setSelectedPage('projects')} />
+            <AnchorLink className={`${selectedPage === 'resume' ? selectedStyle : 'bg-pale-purple'} w-3 h-3 rounded-full`} href='#resume' onClick={() => setSelectedPage('resume')} />
+            <AnchorLink className={`${selectedPage === 'contact' ? selectedStyle : 'bg-pale-purple'} w-3 h-3 rounded-full`} href='#contact' onClick={() => setSelectedPage('contact')} />
         </div>
     )
 }
