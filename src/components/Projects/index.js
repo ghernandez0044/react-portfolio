e6dc1e4008b02c80967e5c6a6c67f8486cd8a159
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-19 22:09:23 -0700

    progress check, still working on projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,6 +6,16 @@ function Projects(){
     return (
         <section id='projects' className="pt-48 pb-48">
             {/* HEADINGS */}
+            <motion.div className="md:w-1/3" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.9 }} variants={{ hidden: { opacity: 0, x: -50 },
+                       visible: { opacity: 1, x: 0 } }}>
+                <p className="font-subheading font-semibold text-4xl mb:-5">
+                    My <span className="text-pale-purple">Skills</span>
+                </p>
+                <LineGradient width="w-1/3" />
+                <p className="mt-10 mb-7">
+                    A list of programming languages and frameworks I have experience building projects with
+                </p>
+            </motion.div>
         </section>
     )
 }
