042b3138fe7afce1576ebf098b402d58f6a01b36
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-20 11:43:57 -0700

    progress check, still working on projects component

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,10 +3,24 @@ import { motion } from "framer-motion"
 
 function Projects(){
 
+    const container = {
+        hidden: {},
+        visible: {
+            transition: {
+                staggerChildren: 0.2
+            }
+        }
+    }
+
+    const projectVariant = {
+        hidden: { opacity: 0, scale: 0.8 },
+        visible: { opacity: 1, scale: 1 }
+    }
+
     return (
         <section id='projects' className="pt-48 pb-48">
             {/* HEADINGS */}
-            <motion.div className="md:w-2/5 mx-auto text-center" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.9 }} variants={{ hidden: { opacity: 0, y: -50 },
+            <motion.div className="md:w-2/5 mx-auto text-center" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, y: -50 },
                        visible: { opacity: 1, y: 0 } }}>
                 <div>
                     <p className="font-subheading font-semibold text-4xl uppercase">
@@ -22,6 +36,13 @@ function Projects(){
             </motion.div>
 
             {/* PROJECTS */}
+            <div className="flex justify-center">
+                <motion.div className="sm:grid sm:grid-cols-3" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} variants={container}>
+                    <div className="flex justify-center text-center items-center p-10 bg-firengine-red max-w-[400px] max-h-[400px] text-2xl font-subheading font-semibold">
+                        Beautiful User Interfaces
+                    </div>
+                </motion.div>
+            </div>
         </section>
     )
 }
