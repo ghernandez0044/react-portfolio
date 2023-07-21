6113ff2515363bb80f2a95b9d0a5dd68c9978600
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-21 01:49:02 -0700

    double checking projects and project components

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion"
 function Project({ title, projectVariant, source }){
 
     const projectTitle = title.split(" ").join("-").toLowerCase()
-    const overlayStyles = 'absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-space-blue'
+    const overlayStyles = 'absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-pale-purple z-30 flex flex-col justify-center items-center text-center p-16 text-space-blue cursor-pointer'
 
     return (
         <motion.div variants={projectVariant} className="relative">
