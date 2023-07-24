0b833c2fd4113c4ede520b471db0809895ca7d74
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-24 01:29:23 -0700

    progress check, still working on contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -15,7 +15,7 @@ function Contact(){
     return (
         <section id='contact' className="py-48">
             {/* HEADINGS */}
-            <motion.div initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}>
+            <motion.div className="basis-1/2 flex justify-center" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}>
                 <div>
                     <p className="uppercase font-subheading font-semibold text-4xl">
                         <span className="text-tiffany-blue">Contact Me</span>
@@ -25,6 +25,18 @@ function Contact(){
                     </div>
                 </div>
             </motion.div>
+
+            {/* FORM & IMAGE */}
+            <div className="md:flex md:justify-between gap-16 mt-5">
+                <motion.div className="basis-1/2 mt-10 md:mt-0" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, y: 50 }, visible: { opacity: 1, y: 0 } }}>
+                    <img src="https://images.pexels.com/photos/821754/pexels-photo-821754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="contact" />
+                </motion.div>
+                <motion.div initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5, delay: 0.2 }} variants={{ hidden: { opacity: 0, y: 50 }, visible: { opacity: 1, y: 0 } }}>
+                    <form target="_blank" onSubmit={onSubmit} action="" method="POST">
+                        
+                    </form>
+                </motion.div>
+            </div>
         </section>
     )
 }
