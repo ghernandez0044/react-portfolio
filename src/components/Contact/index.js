ef2a400338df84dc7a2cc15f9f4638d632fa5df8
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-24 01:06:47 -0700

    progress check, working on contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,9 +3,22 @@ import { useForm } from 'react-hook-form'
 import { motion } from "framer-motion";
 
 function Contact(){
+    const { register, trigger, formState: {errors} } = useForm()
+
+    const onSubmit = async (e) => {
+        const isValid = await trigger()
+        if(!isValid){
+            e.preventDefault()
+        }
+    }
 
     return (
-        null
+        <section id='contact' className="py-48">
+            {/* HEADINGS */}
+            <motion.div className="md:w-1/3 text-center md:text-left" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}>
+                
+            </motion.div>
+        </section>
     )
 }
 
