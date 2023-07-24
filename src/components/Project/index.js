663fed1a2e382315b5eb1d8e4aa89c7d4082fd18
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-24 00:31:04 -0700

    progress check, still working on projects component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion"
 
-function Project({ title, projectVariant, source, subtitle }){
+function Project({ title, projectVariant, source }){
 
     const projectTitle = title.split(" ").join("-").toLowerCase()
     const overlayStyles = 'absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-pale-purple z-30 flex flex-col justify-center items-center text-center p-16 text-space-blue cursor-pointer'
@@ -8,9 +8,8 @@ function Project({ title, projectVariant, source, subtitle }){
     return (
         <motion.div variants={projectVariant} className="relative">
             <div className={overlayStyles}>
-                <p className="text-2xl font-subheading">{title}</p>
-                <p className="mt-7">
-                    {subtitle} 
+                <p className="text-3xl font-subheading font-semibold">{title}</p>
+                <p className="mt-7"> 
                 </p>
             </div>
             <img src={source ? source : `../assets/${projectTitle}.png`} alt={projectTitle} />
