0f97bef5218002d24bd078293fc1b9dd4245d589
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-24 00:55:54 -0700

    progress check, working on contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,2 +1,12 @@
 import LineGradient from "../LineGradient";
 import { useForm } from 'react-hook-form'
+import { motion } from "framer-motion";
+
+function Contact(){
+
+    return (
+        null
+    )
+}
+
+export default Contact
