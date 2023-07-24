0420cf713ee3bb7ce8163d5e75700aaae9c3abbd
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-24 01:11:35 -0700

    progress check, still working on contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -15,8 +15,16 @@ function Contact(){
     return (
         <section id='contact' className="py-48">
             {/* HEADINGS */}
-            <motion.div className="md:w-1/3 text-center md:text-left" initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}>
-                
+            <motion.div initial='hidden' whileInView='visible' viewport={{ once: true, amount: 0.5 }} transition={{ duration: 0.5 }} variants={{ hidden: { opacity: 0, x: -50 }, visible: { opacity: 1, x: 0 } }}>
+                <div>
+                    <p className="uppercase font-subheading font-semibold text-4xl mb-5 text-firengine-red">
+                        Contact Me
+                    </p>
+                    <LineGradient width="mx-auto w-2/4" />
+                    <p className="mt-10">
+                        Here is what people are saying
+                    </p>
+                </div>
             </motion.div>
         </section>
     )
