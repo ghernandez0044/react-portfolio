5118614e7e5bffada7eae2c58d018a4a14482610
ghernandez0044 <g.hernandez0044@yahoo.com>
2023-07-24 23:56:03 -0700

    progress check

--- src/components/SocialMediaIcons/index.js
+++ src/components/SocialMediaIcons/index.js
@@ -1,6 +1,5 @@
 function SocialMediaIcons(){
 
-
     return (
         <div className="flex justify-center md:justify-start my-10 gap-7">
             <a className="hover:opacity-50 transition duration-700 font-heading uppercase hover:text-pale-purple" href="https://www.linkedin.com" target="_blank" rel="noreferrer">
